f05e007c1adfbab449283471fb76673d265dc6d4
MilanJulinac42 <milanjulinac996@gmail.com>
2024-09-30 15:58:23 +0200

    shift+enter now works properly in all cases

---
+++ packages/cms/src/util/traversals/moveCursorToEnd.ts
@@ -0,0 +1,8 @@
+export const moveCursorToEnd = (element: HTMLElement) => {
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNodeContents(element);
+    range.collapse(false);
+    selection?.removeAllRanges();
+    selection?.addRange(range);
+};
