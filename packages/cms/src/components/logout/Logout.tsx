a2404d15e7dcf59a285d96ed5a2c780fdb80fb27
Mijura <miodragvilotijevic@gmail.com>
2024-10-04 16:46:04 +0200

    removed boilerplate code

--- packages/cms/src/components/logout/Logout.tsx
+++ packages/cms/src/components/logout/Logout.tsx
@@ -1,34 +1,30 @@
-import React from 'react';
-import { signOut } from 'aws-amplify/auth';
-import { useNavigate } from '@tanstack/react-router';
-import { useDispatch } from 'react-redux';
-import { accountUpdated } from '../../reducers/authReducer';
+import React from 'react'
+import { signOut } from 'aws-amplify/auth'
+import { useNavigate } from '@tanstack/react-router'
+import { useDispatch } from 'react-redux'
+import { accountUpdated } from '../../reducers/authReducer'
 
 const LogoutButton = () => {
-    const navigate = useNavigate();
+    const navigate = useNavigate()
     const dispatch = useDispatch()
 
     const handleLogout = async () => {
-        console.log('LOG Logout button clicked');
         try {
-            console.log('LOG Attempting to sign out...');
-            await signOut();
-            console.log('LOG Sign out successful');
+            await signOut()
             dispatch(accountUpdated({
                 authorized: false,
                 account: null
             }))
-            navigate({ to: '/' });
-            console.log('LOG Navigation attempted');
+            navigate({ to: '/' })
         } catch (error) {
-            console.error('LOG Error signing out: ', error);
+            console.error('LOG Error signing out: ', error)
         }
-    };
+    }
     return (
         <button onClick={handleLogout}>
             Logout
         </button>
-    );
-};
+    )
+}
 
 export default LogoutButton;
