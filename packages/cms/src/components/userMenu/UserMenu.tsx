78fe432e641045421800f7b36a7dfd7b0598b770
Mijura <miodragvilotijevic@gmail.com>
2024-10-04 12:18:48 +0200

    user icon update

--- packages/cms/src/components/userMenu/UserMenu.tsx
+++ packages/cms/src/components/userMenu/UserMenu.tsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect, useRef } from 'react'
 import { signOut, getCurrentUser } from 'aws-amplify/auth'
 import { useNavigate } from '@tanstack/react-router'
 import { ExpandDownIcon } from '../../icons/ExpandDownIcon'
-import { LogoIcon } from '../../icons/LogoIcon'
 import { createPortal } from 'react-dom'
 import ClickOutsideListener from '../popover/ClickOutsideListener'
 import { blockingUpdated } from '../../reducers/toolbarReducer'
 import { useDispatch } from 'react-redux'
+import { InitialIcon } from '../../icons/InitialIcon'
 
 const UserMenu = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -67,24 +67,24 @@ const UserMenu = () => {
                     onClick={handleOpen}
                     className="p-2 flex items-center justify-between text-gray-800 hover:bg-black-50 transition-colors duration-200"
                 >
-                    <div className="flex flex-row items-center space-x-2 hover:bg-primary-light hover:bg-opacity-40 p-2 rounded-md" ref={ref}>
-                        <LogoIcon />
+                    {userEmail && <div className="flex flex-row items-center space-x-2 hover:bg-primary-light hover:bg-opacity-60 p-2 rounded-md" ref={ref}>
+                        <InitialIcon initials={userEmail[0].toUpperCase()}/>
                         <ExpandDownIcon />
-                    </div>
+                    </div>}
                 </button>
 
 
                 {
                     isOpen && createPortal(<ClickOutsideListener callback={handleClose}>
 
-                        <div className={`fixed flex rounded-md p-1 p-2 shadow bg-[white] w-[250px]`}
+                        <div className={`fixed flex rounded-md p-1 shadow bg-[white] w-[250px]`}
                             style={{ top: rect.top + rect.height, left: rect.x }}>
-                            <div className="flex flex-col gap-2 w-full">
-                                <span className="font-medium text-sm truncate">{userEmail}</span>
+                            <div className="flex flex-col gap-1 w-full">
+                                <span className="p-1 px-2 font-medium text-sm truncate">{userEmail}</span>
                                 <div className='border-b border-primary' />
                                 <button
                                     onClick={handleLogout}
-                                    className="p-2 text-left text-sm text-gray-700 transition-colors duration-200 hover:bg-primary-light rounded-md"
+                                    className="p-1 px-2 text-left text-sm text-gray-700 transition-colors duration-200 hover:bg-primary-light rounded-md"
                                 >
                                     Logout
                                 </button>
