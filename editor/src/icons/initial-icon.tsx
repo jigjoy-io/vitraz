0ec6e9fb614289f48677faaebdb99737cb9a2244
Mijura <miodragvilotijevic@gmail.com>
2026-02-05 01:42:22 +0100

    removed unused icons

--- editor/src/icons/initial-icon.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function InitialIcon({ initials }) {
-	return (
-		<div className="rounded-[5px] h-[32px] w-[32px] flex items-center bg-primary justify-center">
-			<p className="flex justify-center items-center text-white text-paragraph">{initials}</p>
-		</div>
-	)
-}
